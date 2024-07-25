b4d8b2a12649b68b5be33d5ea87432786d2ef82c
Allexandrres <alexandredasilveira13@gmail.com>
2024-07-25 12:25:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+//function saudacao(nome){
+//    alert(`olá ${nome}`);
+//}
+
+//function chamadacallback(callback) {
+//    let nome = prompt('pro favor insira seu nome')
+//    callback(nome);
+//}
+//chamadacallback(saudacao);
+
+//const numero = parseInt("3.14")
+//console.log(numero);
+//const decimal = parseFloat(42);
+//console.log(decimal);
+//Math.PI = 3.141592653589793;
+//console.log(PI.toFixed(2));
+
+//----------------------------------------------------------------------------------//
+//atividade 01
+
+//const multiplicar = (a, b) => a * b;
+
+//console.log(multiplicar(3 , 5)); 
+//console.log(multiplicar(10, 7));
+
+//const nomefuction = (argumento1, argumento2) => expressão;
+
+//----------------------------------------------------------------------------------//
+
+//atividade 02
+
+//const contarvogais = str => {
+//    const vagais = 'aeiouaeiou';
+//    let count = 0;
+
+//    for (let char of str) {
+//        if (vogais.includes(char)){
+//            count++;
+//        }
+//    }
+// return count;
+//};
+
+//-----------------------------------------------------------------------------------//
+
+//ativiade 03
+
+//function convertertemperatura(temperaturacelsius,callbackfn){
+//    return callbackfn(temperaturacelsius)
+//}
+
+//-------------------------------------------------------------------------------------//
+
+//atividade 04
+
+//const calcularCubo = num => num ** 3;
+
+//const processarNumero = (num, callback) => {
+//  return callback(num);
+//};
+
+//const numero = 3;
+//const resultado = processarNumero(numero, calcularCubo);
+
+//console.log(`O cubo de ${numero} é ${resultado}`);
